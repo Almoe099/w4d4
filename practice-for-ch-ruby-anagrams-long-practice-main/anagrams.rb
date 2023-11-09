ae33d8946fa19754830c684d0c105afe631a2401
Kaefa Kanneh <kaefakanneh@hotmail.com>
2023-11-09 16:59:50 -0500

    Add fourth anagram

--- practice-for-ch-ruby-anagrams-long-practice-main/anagrams.rb
+++ practice-for-ch-ruby-anagrams-long-practice-main/anagrams.rb
@@ -23,3 +23,28 @@ end
 
 p third_anagram?("gizmo", "sally")    #=> false
 p third_anagram?("elvis", "lives")    #=> true
+
+def fourth_anagram?(str1, str2)
+    str1_freq = str1.chars.tally
+    str2_freq = str2.chars.tally
+
+    str1_freq == str2_freq
+end
+
+
+
+p fourth_anagram?("gizmo", "sally")    #=> false
+p fourth_anagram?("elvis", "lives")    #=> true
+
+def bonus_fourth_anagram?(str1, str2)
+  count = Hash.new(0)
+
+  str1.split("").each {|char| count[char] += 1 } 
+  str2.split("").each {|char| count[char] -= 1 }
+  count.all? {|_, v| v.zero? }
+end
+
+
+
+p bonus_fourth_anagram?("gizmo", "sally")    #=> false
+p bonus_fourth_anagram?("elvis", "lives")    #=> true
